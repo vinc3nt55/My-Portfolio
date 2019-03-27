fbcffb5eafd30adc37bd6b6da45c542d627e3fa8
vinc3nt55 <vinc3nt55@gmail.com>
2019-03-27 21:53:52 +0800

    Updates

---
+++ precache-manifest.147eeff000a2e8f10f5b7339c584dccd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "291e7d04bba1e680b6639e757956d83a",
+    "url": "/My-Portfolio/static/media/skills.291e7d04.mp4"
+  },
+  {
+    "revision": "414c0871e8bce6304257901606a61cb0",
+    "url": "/My-Portfolio/static/media/portfolio.414c0871.mp4"
+  },
+  {
+    "revision": "2f03aa8a5249fa5085289a9b6e83acf1",
+    "url": "/My-Portfolio/static/media/contact.2f03aa8a.mp4"
+  },
+  {
+    "revision": "ed95869238e5b17c9b0d2d1146115455",
+    "url": "/My-Portfolio/static/media/avatar.ed958692.png"
+  },
+  {
+    "revision": "3d6774adc388f60dc65ff74f40e1d44c",
+    "url": "/My-Portfolio/static/media/about.3d6774ad.mp4"
+  },
+  {
+    "revision": "b7d4bf292e75006047a4",
+    "url": "/My-Portfolio/static/js/runtime~main.f7066801.js"
+  },
+  {
+    "revision": "acf4b56a4273b2c9c4bb",
+    "url": "/My-Portfolio/static/js/main.2508d4e2.chunk.js"
+  },
+  {
+    "revision": "a133b710efd5495a2f98",
+    "url": "/My-Portfolio/static/js/2.d1f71249.chunk.js"
+  },
+  {
+    "revision": "acf4b56a4273b2c9c4bb",
+    "url": "/My-Portfolio/static/css/main.705a8ae2.chunk.css"
+  },
+  {
+    "revision": "e1bedad9aa5d80a6fe04334b3745a6a5",
+    "url": "/My-Portfolio/index.html"
+  }
+];
